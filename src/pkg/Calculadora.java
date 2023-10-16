fa780644a4bec4e338a6bb07b1f4ceaff4293017
Jiuso <jesusgg200x@gmail.com>
2023-10-16 22:23:39 +0200

    Paso 2 del TDD. La clase Calculadora devuelve inmediatamente lo esperado en las pruebas, aunque todavía la funcionalidad no se ha implementado.

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -3,15 +3,15 @@ package pkg;
 public class Calculadora {
 
 	public static Integer suma(int i, int j) {
-		return 0;
+		return 5;
 	}
 
 	public static Integer resta(int i, int j) {
-		return 0;
+		return -1;
 	}
 
 	public static Integer multiplica(int i, int j) {
-		return 0;
+		return 6;
 	}
 	
 	public static Integer divide(int i, int j) {
