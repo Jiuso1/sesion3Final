3716f5732901d500e4eb689024d349cd70280878
Jiuso <jesusgg200x@gmail.com>
2023-10-17 11:15:36 +0200

    Paso 3 del TDD. Refactorizada la clase Cuenta.

--- src/pkg/Cuenta.java
+++ src/pkg/Cuenta.java
@@ -3,6 +3,8 @@ package pkg;
 import java.util.ArrayList;
 import java.util.List;
 
+import pkg.Movimiento.Signo;
+
 public class Cuenta {
 	
 	private String numero;
@@ -33,13 +35,32 @@ public class Cuenta {
 	public void setmMovimientos(List<Movimiento> mMovimientos) {
 		this.mMovimientos = mMovimientos;
 	}
-
-	public void ingresar(double i) {
-		this.setSaldo(this.getSaldo() + i);
+	
+	public String getNumero() {
+		return this.numero;
 	}
 	
-	public void retirar(double i) {//Consideramos que la cuenta tiene crédito infinito.
-		this.setSaldo(this.getSaldo() - i);
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public void ingresar(double i) {
+		Movimiento movimiento = new Movimiento(i, Signo.H, "Ingreso");
+		mMovimientos.add(movimiento);//Añado el movimiento.
+		this.saldo += i;//Incremento el saldo.
+		System.out.println("Ingreso de "+i+"€");
+	}
+	
+	public void retirar(double i) {
+		//Debemos tener en cuenta el límite de descubierto de 500€.
+		if (saldo - i >= -500) {
+			Movimiento movimiento = new Movimiento(i, Signo.D, "Retirada");
+			mMovimientos.add(movimiento);
+			saldo -= i;
+			System.out.println("Retirada de "+i+"€");
+		} else {
+			System.out.println("Fondos insuficientes (saldo "+this.saldo+"€) en la cuenta "+this.numero+" para el reintegro de "+i+"€ (x)");
+		}
 	}
 	
 }
